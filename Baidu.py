00e5dbf9bd3c6ec932d6e18047e67054fa4c6b80
takeru0219 <58388940+takeru0219@users.noreply.github.com>
2020-04-29 15:34:02 +0900

    Add files via upload

---
+++ Baidu.py
@@ -0,0 +1,48 @@
+import slackweb
+from bs4 import BeautifulSoup
+import requests
+
+# 取得 -> BeautifulSoupに渡す
+url = 'http://top.baidu.com/buzz?b=341&fr=topbuzz_b342'
+response = requests.get(url)
+
+soup = BeautifulSoup(response.content, 'html.parser')
+
+# soup内の処理
+data_array_keyword = soup.find_all(class_ = 'list-title')
+data_array_score = soup.find_all(class_ = 'last')
+
+words = []
+urls = []
+scores = []
+
+for keyword_url, score in zip(data_array_keyword[0:10], data_array_score[2:12]):
+    words.append(keyword_url.text)
+    urls.append(keyword_url.get('href'))
+    scores.append(score.text.strip())
+
+# Slackに流す
+slack = slackweb.Slack(url = 'https://hooks.slack.com/services/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')
+attachments = []
+
+for word, url, score in zip(words, urls, scores):
+    if int(score) >= 100000:
+        color = 'danger'
+    elif int(score) >= 40000:
+        color = 'warning'
+    else:
+        color = 'good'
+    attachments.append(
+    {
+        # 'fallback' : '今日热点事件排行榜',
+        'title' : word,
+        'title_link' : url,
+        'text' : score,
+        'color' : color
+    })
+
+slack.notify(
+    text = '今日热点事件排行榜', 
+    username = '百度热点',
+    attachments = attachments
+)
